76b69a4fee327de5aea92ca29f3d5fe46aa05492
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-01-16 15:46:25 +0500

    chore: react router basename fix

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,21 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import WalletWizardPage from './pages/WalletWizard';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/wallet-wizard',
+      element: <WalletWizardPage />,
+    },
+  ],
   {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/wallet-wizard',
-    element: <WalletWizardPage />,
-  },
-]);
+    basename: import.meta.env.VITE_BASENAME,
+  }
+);
 
 export function Router() {
   return <RouterProvider router={router} />;
